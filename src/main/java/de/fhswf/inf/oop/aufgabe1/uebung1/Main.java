e2f7adab5ca9986f5d5679616a2fa958eed9f26d
M. Faulstich <faulstich.matthias@fh-swf.de>
2026-03-24 15:52:06 +0100

    Kommentare Main, Aufgabe 1 Übung 1 zugefügt.

--- src/main/java/de/fhswf/inf/oop/aufgabe1/uebung1/Main.java
+++ src/main/java/de/fhswf/inf/oop/aufgabe1/uebung1/Main.java
@@ -2,6 +2,9 @@ package de.fhswf.inf.oop.aufgabe1.uebung1;
 
 import java.util.function.Function;
 
+/**
+ * Aufgabe 1 Übung 1: Umgang mit Bitmasken
+ */
 public class Main {
 
 	private static final byte OUTPUT_MOTOR_RIGHT = 1;
@@ -10,7 +13,12 @@ public class Main {
 	private static final byte LIMIT_RIGHT = 16;
 	private static final byte LIMIT_LEFT = 32;
 
-
+	/**
+	 * Startmethode der Anwendung
+	 * 
+	 * @param args Übergebene Argumente beim Starten auf einer Konsole. Nicht
+	 *             verwendet.
+	 */
 	public static void main(String[] args) {
 		// Setze Ausgangssituation
 		byte value = LIMIT_RIGHT;
@@ -22,7 +30,14 @@ public class Main {
 		System.out.printf("Limit right: %s\n", (value &= LIMIT_RIGHT) != 0 ? "On" : "Off");
 	}
 
-	private static String toBinaryString(byte value) {
+	/**
+	 * Binärwert eines byte als String
+	 * 
+	 * @param value Wert, dessen Bitmuster zurückgegeben wird.
+	 * @return 8 Bits von value als String: "00000000". Führenden "0"-Werte auf 8
+	 *         Binärstellen aufgefüllt.
+	 */
+	public static String toBinaryString(byte value) {
 		String result = Integer.toBinaryString(Integer.valueOf(value));
 		while (result.length() < 8) {
 			result = "0" + result;
