4daa0eaae094c0bbdbbb6572ff7b4166aa9e501b
Matthias Faulstich <105288604+mfaulstich@users.noreply.github.com>
2026-04-01 07:29:26 +0000

    Aufgabe 7.1 Geometrische Formen, fertig.

---
+++ src/main/java/de/fhswf/inf/oop/aufgabe7/uebung1/Circle.java
@@ -0,0 +1,35 @@
+package de.fhswf.inf.oop.aufgabe7.uebung1;
+
+public class Circle extends Shape {
+    private double radius;
+
+    /**
+     * Initialisierungskonstruktor.
+     * 
+     * @param radius Radius des Kreises. Muss ein Wert >= 0.0 sein.
+     */
+    public Circle(double radius) {
+        if (radius < 0.0) {
+            throw new IllegalArgumentException(
+                    String.format("Erwarte positiven Wert für den Radius, ist aber %.2f", radius));
+        }
+        this.radius = radius;
+
+    }
+
+    /**
+     * @return Fläche des Kreises.
+     */
+    @Override
+    public double area() {
+        return radius * radius * Math.PI;
+    }
+
+    /**
+     * @return Beschreibung der Geometrie und der Kenngrößen
+     */
+    @Override
+    public String toString() {
+        return String.format("Kreis, r=%.2f", radius);
+    }
+}
