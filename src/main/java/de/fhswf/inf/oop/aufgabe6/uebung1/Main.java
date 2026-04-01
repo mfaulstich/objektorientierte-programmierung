9660edb4315caa97340b40fc515a814bd31ea3a3
Matthias Faulstich <105288604+mfaulstich@users.noreply.github.com>
2026-04-01 06:19:17 +0000

    Aufgabe 6.1, Student, fertig

---
+++ src/main/java/de/fhswf/inf/oop/aufgabe6/uebung1/Main.java
@@ -0,0 +1,19 @@
+package de.fhswf.inf.oop.aufgabe6.uebung1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Student("Peter Pan", 19, "01234567"));
+        System.out.println(new Student("Lisa Schlupke", 21, "12345678"));
+        try {
+            System.out.println(new Student("Lisa Schlupke", 21, "A2345678"));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println(new Student("Lisa Schlupke", 21, "1234567X"));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
