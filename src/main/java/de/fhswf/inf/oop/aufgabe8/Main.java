b2761a744b887ff953d6139378422daffd624abd
M. Faulstich <faulstich.matthias@fh-swf.de>
2026-05-06 08:37:58 +0200

    Ergänzung Aufgabe 8

--- src/main/java/de/fhswf/inf/oop/aufgabe8/Main.java
+++ src/main/java/de/fhswf/inf/oop/aufgabe8/Main.java
@@ -1,36 +1,42 @@
 package de.fhswf.inf.oop.aufgabe8;
 
-import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.PKW;
-import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.Verbrenner;
 import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.E_PKW;
 import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.Fahrer;
 import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.Fahrzeug;
 import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.Motor;
+import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.PKW;
+import de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge.Verbrenner;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        /*
-         * Übung 1
-         */
-        Fahrer fahrer = new Fahrer();
-        Fahrzeug f1 = new Fahrzeug("VW", 2025);
-        Fahrzeug f2 = new Fahrzeug("Audi", 2023);
-        Fahrzeug f3 = new PKW("Opel", 2023, new Motor("Frrrrr"), fahrer);
-        Fahrzeug f4 = new PKW("Renault", 2023, new Motor("Frrrrr"), fahrer);
-        System.out.println(f1);
-        System.out.println(f2);
-        System.out.println(f3);
-        System.out.println(f4);
+		/*
+		 * Übung 1
+		 */
+		Fahrer fahrer = new Fahrer();
+		Fahrzeug f1 = new Fahrzeug("VW", 2025);
+		Fahrzeug f2 = new Fahrzeug("Audi", 2023);
+		Fahrzeug f3 = new PKW("Opel", 2023, new Motor("Frrrrr"), fahrer);
+		Fahrzeug f4 = new PKW("Renault", 2023, new Motor("Frrrrr"), fahrer);
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(f3);
+		System.out.println(f4);
 
-        // new Fahrzeug("KIA", 2019).getServiceInterval();
-        new PKW("KIA", 2019, new Motor("Brummm"), fahrer).getServiceInterval();
+		// new Fahrzeug("KIA", 2019).getServiceInterval();
+		new PKW("KIA", 2019, new Motor("Brummm"), fahrer).getServiceInterval();
 
-        System.out.println(new E_PKW("VW", 2026, fahrer).getAntrieb());
-        System.out.println(new Verbrenner("VW", 2026, fahrer).getAntrieb());
+		System.out.println(new E_PKW("VW", 2026, fahrer).getAntrieb());
+		System.out.println(new Verbrenner("VW", 2026, fahrer).getAntrieb());
 
-        System.out.printf("Fahrzeuge anglegt: %3d\n", Fahrzeug.getCount());
+		System.out.printf("Fahrzeuge anglegt: %3d\n", Fahrzeug.getCount());
 
-        System.out.print("Starte..  " + ((PKW)f4).starten());
-    }
+		System.out.print("Starte..  " + ((PKW) f4).starten());
+
+		Motor motor = new Motor("Brumm");
+		// Funktioniert nicht, weil Motor.zuenden() Package-private ist und nur von
+		// Objekten von Klassen in de.fhswf.inf.oop.aufgabe8.uebung1.fahrzeuge
+		// erreichbar ist.
+		// motor.zuenden();
+	}
 }
